cab4280a50905d4d19598dcf49b524f783152eec
sumit-bhatt9 <132207043+sumit-bhatt9@users.noreply.github.com>
2023-11-22 19:43:23 +0530

    Add files via upload

---
+++ api-gateway/src/main/java/com/springboot/sms/apigateway/ApiGatewayApplicatiion.java
@@ -0,0 +1,11 @@
+package com.springboot.sms.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplicatiion {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplicatiion.class,args);
+    }
+}
